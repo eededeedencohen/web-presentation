9e6d70c68dcc1e1d6451692af847329a8e706136
Eden Cohen <eededeedencohen@gmail.com>
2025-12-14 10:40:32 +0200

    fix: integrate CircularShadowImage and fix imports

--- src/pages/Slide/Slide.jsx
+++ src/pages/Slide/Slide.jsx
@@ -1,17 +1,22 @@
 // pages/Slide/Slide.jsx
-import React from 'react';
-import styles from './Slide.module.css';
+import React from "react";
+import styles from "./Slide.module.css";
 
 // ייבוא הבלוקים
-import HeaderBlock from '../../components/HeaderBlock/HeaderBlock';
-import MainBlock from '../../components/MainBlock/MainBlock';
-import SideBlock from '../../components/SideBlock/SideBlock';
-import ShadowCard from '../../components/ShadowCard/ShadowCard';
+import HeaderBlock from "../../components/HeaderBlock/HeaderBlock";
+import MainBlock from "../../components/MainBlock/MainBlock";
+import SideBlock from "../../components/SideBlock/SideBlock";
+import ShadowCard from "../../components/ShadowCard/ShadowCard";
+
+// ייבוא הרכיב המתוקן
+import CircularShadowImage from "../../components/CircularShadowImage/CircularShadowImage";
+
+// ייבוא התמונה (וודא שהנתיב הזה אכן נכון במבנה התיקיות שלך)
+import exampleImage from "../../assets/Images/image.png";
 
 export default function Slide() {
   return (
     <div className={styles.slidePage}>
-      
       {/* אזור הכותרת */}
       <div className={styles.headerArea}>
         <HeaderBlock />
@@ -24,11 +29,22 @@ export default function Slide() {
 
       {/* האזור הראשי */}
       <div className={styles.mainArea}>
-        <MainBlock >
+        <MainBlock>
           <ShadowCard />
+          {/* שימוש ברכיב התמונה העגולה */}
+          <CircularShadowImage
+            src={exampleImage}
+            alt="Example View"
+            color="var(--purple)"
+            size="200px"
+            offsetX={6}
+            offsetY={8}
+            spread={9}
+            className={styles.extraDesign}
+            onClick={() => alert("Hi!")}
+          />
         </MainBlock>
       </div>
-
     </div>
   );
 }
