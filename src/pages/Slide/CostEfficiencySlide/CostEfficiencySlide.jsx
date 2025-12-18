5b3c8194ce737ca22c89afa172612165e7eebb16
Eden Cohen <eededeedencohen@gmail.com>
2025-12-18 16:17:22 +0200

    feat: create CostEfficiencySlide page

--- src/pages/Slide/CostEfficiencySlide/CostEfficiencySlide.jsx
+++ src/pages/Slide/CostEfficiencySlide/CostEfficiencySlide.jsx
@@ -2,7 +2,35 @@ import React from "react";
 import styles from "./CostEfficiencySlide.module.css";
 import HeaderBlock from "../../../components/HeaderBlock/HeaderBlock";
 import CostSavingCard from "../../../components/Cards/CostSavingCard/CostSavingCard";
-
+import hagadaImage from "../../../assets/Images/hagada.jpg";
+import notebooksImage from "../../../assets/Images/notebooks.png";
+import bagsImage from "../../../assets/Images/bags.png";
+const examplesData = [
+  {
+    id: 1,
+    title: "דוגמה 1: הגדה של פסח (500 יח')",
+    subtitle: "הבחירה האופטימלית חוסכת לנו 42% בעלויות.",
+    priceHigh: 11500,
+    priceLow: 6650,
+    image: hagadaImage,
+  },
+  {
+    id: 2,
+    title: "דוגמה 2: שקיות כותנה ואלבד (500 יח')",
+    subtitle: "הבחירה האופטימלית חוסכת לנו 61% בעלויות.",
+    priceHigh: 6500,
+    priceLow: 2500,
+    image: bagsImage,
+  },
+  {
+    id: 3,
+    title: "דוגמה 3: מחברות A5 (1000 יח')",
+    subtitle: "הבחירה האופטימלית חוסכת לנו 37% בעלויות.",
+    priceHigh: 15900,
+    priceLow: 11500,
+    image: notebooksImage,
+  },
+];
 function CostEfficiencySlide() {
   return (
     <div className={styles.slidePage}>
@@ -24,13 +52,34 @@ function CostEfficiencySlide() {
       </div>
       <div className={styles.cardsArea}>
         <div className={styles.cardWrapper}>
-          <CostSavingCard />
+          <CostSavingCard
+            image={examplesData[0].image}
+            contentProduct={examplesData[0].title}
+            contentSavings={examplesData[0].subtitle}
+            highestPrice={examplesData[0].priceHigh}
+            lowestPrice={examplesData[0].priceLow}
+            className={styles.firstCard}
+          />
         </div>
         <div className={styles.cardWrapper}>
-          <CostSavingCard />
+          <CostSavingCard
+            image={examplesData[1].image}
+            contentProduct={examplesData[1].title}
+            contentSavings={examplesData[1].subtitle}
+            highestPrice={examplesData[1].priceHigh}
+            lowestPrice={examplesData[1].priceLow}
+            className={styles.secondCard}
+          />
         </div>
         <div className={styles.cardWrapper}>
-          <CostSavingCard />
+          <CostSavingCard
+            image={examplesData[2].image}
+            contentProduct={examplesData[2].title}
+            contentSavings={examplesData[2].subtitle}
+            highestPrice={examplesData[2].priceHigh}
+            lowestPrice={examplesData[2].priceLow}
+            className={styles.thirdCard}
+          />
         </div>
       </div>
     </div>
