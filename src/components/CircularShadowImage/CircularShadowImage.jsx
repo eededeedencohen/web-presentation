fb6a140247956eb754d6092507f49305e32ed935
Eden Cohen <eededeedencohen@gmail.com>
2025-12-22 11:37:10 +0200

    fix apple border-box

--- src/components/CircularShadowImage/CircularShadowImage.jsx
+++ src/components/CircularShadowImage/CircularShadowImage.jsx
@@ -1,3 +1,39 @@
+// import React from "react";
+// import styles from "./CircularShadowImage.module.css";
+
+// function CircularShadowImage({
+//   src,
+//   alt = "image",
+//   color = "var(--purple)",
+//   size = "150px",
+//   offsetX = 8,
+//   offsetY = 10,
+//   spread = 1, // הוספנו פרמטר לעובי הצללית (ברירת מחדל 1px)
+//   className = "",
+//   style = {},
+//   ...restProps
+// }) {
+//   const dynamicVars = {
+//     "--img-color": color,
+//     "--img-size": size,
+//     "--offset-x": `${offsetX}px`,
+//     "--offset-y": `${offsetY}px`,
+//     "--shadow-spread": `${spread}px`, // משתנה חדש ל-CSS
+//   };
+
+//   return (
+//     <img
+//       src={src}
+//       alt={alt}
+//       className={`${styles.circularImage} ${className}`}
+//       style={{ ...dynamicVars, ...style }}
+//       {...restProps}
+//     />
+//   );
+// }
+
+// export default CircularShadowImage;
+
 import React from "react";
 import styles from "./CircularShadowImage.module.css";
 
@@ -8,7 +44,7 @@ function CircularShadowImage({
   size = "150px",
   offsetX = 8,
   offsetY = 10,
-  spread = 1, // הוספנו פרמטר לעובי הצללית (ברירת מחדל 1px)
+  spread = 1,
   className = "",
   style = {},
   ...restProps
@@ -18,17 +54,21 @@ function CircularShadowImage({
     "--img-size": size,
     "--offset-x": `${offsetX}px`,
     "--offset-y": `${offsetY}px`,
-    "--shadow-spread": `${spread}px`, // משתנה חדש ל-CSS
+    "--shadow-spread": `${spread}px`,
   };
 
   return (
-    <img
-      src={src}
-      alt={alt}
-      className={`${styles.circularImage} ${className}`}
+    <div
+      className={`${styles.imageWrapper} ${className}`}
       style={{ ...dynamicVars, ...style }}
-      {...restProps}
-    />
+    >
+      <img
+        src={src}
+        alt={alt}
+        className={styles.circularImage}
+        {...restProps}
+      />
+    </div>
   );
 }
 
--- src/pages/Slide/BudgetSummarySlide/BudgetSummarySlide.jsx
+++ src/pages/Slide/BudgetSummarySlide/BudgetSummarySlide.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./BudgetSummarySlide.module.css";
 import HeaderBlock from "../../../components/HeaderBlock/HeaderBlock";
+import IconSVG from "../../../components/IconSVG/IconSVG";
 
 function BudgetSummarySlide() {
   return (
@@ -12,8 +13,33 @@ function BudgetSummarySlide() {
           className={styles.header} // TO DO: Remove this class at the end
         />
       </div>
-      <div className={styles.yearArea}><div className={styles.year}>שנת 2026</div></div>
-      <div className={styles.chartArea}></div>
+      <div className={styles.yearArea}>
+        <div className={styles.year}>שנת 2026</div>
+      </div>
+      <div className={styles.chartArea}>
+        <div className={styles.chartWrapper}>
+          <div className={styles.income}>
+            <div className={styles.incomeStick}>
+              <div className={styles.incomeIcon}>
+                <IconSVG
+                  iconName="income"
+                  color="white"
+                  width="100%"
+                  height="100%"
+                />
+              </div>
+              <div className={styles.incomeLabel}>סך ההכנסות</div>
+            </div>
+          </div>
+          <div className={styles.expenses}>
+            <div className={styles.expensesStick1}>הוצאות</div>
+            <div className={styles.expensesStick2}>הוצאות</div>
+          </div>
+          <div className={styles.profit}>
+            <div className={styles.profitStick}>רווח</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
