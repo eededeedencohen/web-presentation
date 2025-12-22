fbe342c47dc237fb9270fa7557cac9d6e26cadec
Eden Cohen <eededeedencohen@gmail.com>
2025-12-22 11:43:33 +0200

    fix apple border-box bug

--- src/components/CircularShadowImage/CircularShadowImage.jsx
+++ src/components/CircularShadowImage/CircularShadowImage.jsx
@@ -1,39 +1,3 @@
-// import React from "react";
-// import styles from "./CircularShadowImage.module.css";
-
-// function CircularShadowImage({
-//   src,
-//   alt = "image",
-//   color = "var(--purple)",
-//   size = "150px",
-//   offsetX = 8,
-//   offsetY = 10,
-//   spread = 1, // הוספנו פרמטר לעובי הצללית (ברירת מחדל 1px)
-//   className = "",
-//   style = {},
-//   ...restProps
-// }) {
-//   const dynamicVars = {
-//     "--img-color": color,
-//     "--img-size": size,
-//     "--offset-x": `${offsetX}px`,
-//     "--offset-y": `${offsetY}px`,
-//     "--shadow-spread": `${spread}px`, // משתנה חדש ל-CSS
-//   };
-
-//   return (
-//     <img
-//       src={src}
-//       alt={alt}
-//       className={`${styles.circularImage} ${className}`}
-//       style={{ ...dynamicVars, ...style }}
-//       {...restProps}
-//     />
-//   );
-// }
-
-// export default CircularShadowImage;
-
 import React from "react";
 import styles from "./CircularShadowImage.module.css";
 
