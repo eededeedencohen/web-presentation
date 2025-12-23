aabf61dd12f86befcec649d13a636f0eec49fae8
Eden Cohen <eededeedencohen@gmail.com>
2025-12-23 17:24:09 +0200

    deploy2

--- src/pages/Slide/ImpactSummarySlide/ImpactSummarySlide.jsx
+++ src/pages/Slide/ImpactSummarySlide/ImpactSummarySlide.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./ImpactSummarySlide.module.css";
 import HeaderBlock from "../../../components/HeaderBlock/HeaderBlock";
+import SummaryImage from "../../../assets/Images/summary.png";
 
 function ImpactSummarySlide() {
   return (
@@ -21,11 +22,14 @@ function ImpactSummarySlide() {
       <div className={styles.summaryArea}>
         <div className={styles.summaryCardArea}>
           <div className={styles.summaryImageWrapper}>
-            Summary Image Wrapper
+            <img src={SummaryImage} alt="Summary" />
           </div>
           <div className={styles.summarySentenceWrapper}>
-            {" "}
-            Summary Sentence Wrapper
+            <p>
+              תוכנית 2026 היא המנוע שלנו להעצמת אמני שק"ל, להרחבת ההשפעה החברתית
+              שלנו, ולבניית עתיד כלכלי עצמאי. כל שקל של רווח הוא צעד נוסף במימוש
+              החזון המשותף שלנו.
+            </p>
           </div>
         </div>
       </div>
