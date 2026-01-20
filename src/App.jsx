5c228cca5920a68b897bb743fc88ef69de4c0984
Eden Cohen <eededeedencohen@gmail.com>
2026-01-20 09:43:05 +0200

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-// import the OldApp. component:
-import OldApp from "./pages/OldApp/OldApp.jsx";
-// import ShadowCard from './components/ShadowCard/ShadowCard.jsx'
-// import the Slide component
-import Slide from "./pages/Slide/Slide.jsx";
-import VisionSlide from "./pages/Slide/VisionSlide/VisionSlide";
-import StrategySlide from "./pages/Slide/StrategySlide/StrategySlide.jsx";
 import Presentation from "./pages/Slide/Presentation/Presentation.jsx";
-import RoiDawi from "./pages/RoiDawi/RoiDawi.jsx";
 
 function App() {
-  // replace with OldApp component:
-  // return <OldApp />
   return (
     <>
-      {/* <ShadowCard /> */}
-      {/* <OldApp /> */}
-      {/* <Slide /> */}
-      {/* <VisionSlide /> */}
-      {/* <StrategySlide /> */}
-      {/* <Presentation /> */}
-      <RoiDawi />
+      <Presentation />
     </>
   );
 }
-
 export default App;
